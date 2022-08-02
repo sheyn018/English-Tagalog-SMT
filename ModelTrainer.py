bc6223954c7e110120f00c9d03dcc4cae5586903
HakobCob <102731890+HakobCob@users.noreply.github.com>
2022-08-02 22:36:59 +0800

    Add files via upload

--- ModelTrainer.py
+++ ModelTrainer.py
@@ -3,78 +3,91 @@ from nltk.tokenize import word_tokenize
 import IBM1_EM
 import string
 import numpy as np
-import Utils
 
+def sen_tokenizer(plist_sentence, pint_max_trans):
+    lst_final = list()
+    # (value, key)
+    word_dict = {}  # will keep the word and order in its language
+    # (key, value)
+    reverse_dict = {}
+    int_order = 0
+    int_count = 0
 
-def sentence_tokenizer(sentence_list, max_index):
-    final_list = list()
-    word_dictionary = {}    #this dictionary will keep both word and its order in its language
-    reverse_dictionary = {}
-    lang_order = 0
-    cnt = 0
-    translate_table = dict((ord(char), None) for char in string.punctuation)
-    for r in sentence_list[:max_index]:
-        if cnt == 0 :
-            r = r.replace(u'\ufeff', '')
-            cnt += 1
+    # creating a table (33: None, ...) where 33 is the unicode of the characters
+    tbl_translate = dict((ord(char), None) for char in string.punctuation)
 
-        r = r.translate(translate_table)            #remove punctuation
-        tokens = word_tokenize(r.lower())
+    for lst_indiv_sen in plist_sentence[:pint_max_trans]:
+        if int_count == 0:
+            # The \ufeff is only found in the first line. It’s the beginning of the file.
+            lst_indiv_sen = lst_indiv_sen.replace(u'\ufeff', '')
+            int_count += 1
 
-        produced_sentence = ""
-        for token in tokens:
-            if token not in word_dictionary:
-                word_dictionary[token] = lang_order
-                reverse_dictionary[lang_order] = token
-                lang_order += 1
-            produced_sentence = produced_sentence + token + " "
-        produced_sentence = produced_sentence[:(len(produced_sentence) - 1)]  # remove last space
+        # returns a string where some specified characters are replaced with the character described in a dictionary
+        lst_indiv_sen = lst_indiv_sen.translate(tbl_translate)            #remove punctuation
+        lst_tokens = word_tokenize(lst_indiv_sen.lower())
 
-        final_list.append(produced_sentence)
+        str_output = ""
 
-    final_list[0] = final_list[0].replace(u'\ufeff', '')  # ufeff character from document start
-    return final_list, word_dictionary, reverse_dictionary
+        # loop for storing the tokens in the two dictionaries and in the output string
+        for str_token in lst_tokens:
+            if str_token not in word_dict:
+                word_dict[str_token] = int_order
+                reverse_dict[int_order] = str_token
+                int_order += 1
+            # adding the token to the stored string
+            str_output = str_output + str_token + " "
+
+        str_output = str_output[:(len(str_output) - 1)]  # remove last space
+
+        # storing the output in the final list at the end
+        lst_final.append(str_output)
+
+    # replacing the ufeff character from document start with empty string
+    lst_final[0] = lst_final[0].replace(u'\ufeff', '')  # ufeff character from document start
+    return lst_final, word_dict, reverse_dict
 
 
 def model_trainer():
-    with open("English.txt", encoding="utf8") as f:
-        english_data = f.readlines()
-        #print("\n*****\n******\n",english_data)
-        #print("\n*****\n******\n",len(english_data))
+    # Opening the data sets and putting it in an object
+    with open("English.txt", encoding="utf8") as text_file:
+        obj_data_en = text_file.readlines()
 
-    with open("Tagalog.txt", encoding="utf8") as f:
-        tagalog_data = f.readlines()
-        #print("\n*****\n******\n",tagalog_data)
-        #print("\n*****\n******\n",len(tagalog_data))
-        
+    with open("Tagalog.txt", encoding="utf8") as text_file:
+        obj_data_fil = text_file.readlines()
 
-    #just use sentences with length at most 25 words.
-    new_english_data = list()
-    new_tagalog_data = list()
+    # Lists for the new data
+    lst_new_en = list()
+    lst_new_fil = list()
 
-    for sen_marker in range(len(english_data)):
-        if sen_marker > 500000 :
-            break 
-        cur_en_sen = english_data[sen_marker].split()                #tokenizing current sentence
-        cur_tr_sen = tagalog_data[sen_marker].split()
-        new_english_data.append(english_data[sen_marker])
-        new_tagalog_data.append(tagalog_data[sen_marker])
+    for sen_counter in range(len(obj_data_en)):
+        if sen_counter > 500000:
+            break
 
-    english_data = new_english_data.copy()                          # english_data is now a list of tokenized sentence
-    tagalog_data = new_tagalog_data.copy()                          # i.e. a list of sentences where each sentence is a list of words
+        # Splits the sentence word by  word
+        current_en_sen = obj_data_en[sen_counter].split()                #tokenizing current sentence
+        current_fil_sen = obj_data_fil[sen_counter].split()
 
-    #max_num_of_translations = 1000
-    max_num_of_translations = 3000
+        # Adds the sentence at the end of the list
+        lst_new_en.append(obj_data_en[sen_counter])
+        lst_new_fil.append(obj_data_fil[sen_counter])
 
-    # parse tagalog sentences, tokenize the words
-    tagalog_sentences, tagalog_word_dict, reverse_tagalog_word_dict = sentence_tokenizer(tagalog_data, max_num_of_translations)
+    # putting the tokenized data to the data variables
+    # list of sentences where each sentence is a list of words
+    obj_data_en = lst_new_en.copy()                          # obj_data_en is now a list of tokenized sentence
+    obj_data_fil = lst_new_fil.copy()                          # i.e. a list of sentences where each sentence is a list of words
 
-    # parse english sentences, tokenize the words
-    english_sentences, english_word_dict, reverse_english_word_dict = sentence_tokenizer(english_data, max_num_of_translations)
+    # max_trans = 30000
+    max_trans = 3000
+
+    # for parsing the filipino sentences and tokenizing the words
+    lst_fil_sen, fil_word_dict, reverse_fil_dict = sen_tokenizer(obj_data_fil, max_trans)
+
+    # for parsing the english sentences and tokenizing the words
+    lst_en_sen, en_word_dict, reverse_en_dict = sen_tokenizer(obj_data_en, max_trans)
 
  
     #run the EM algorithm of IBM Model 1
-    translate_eng_tagalog  = IBM1_EM.expectation_maximization(tagalog_word_dict,english_word_dict,tagalog_sentences,english_sentences)
+    translate_eng_fil = IBM1_EM.expectation_maximization(fil_word_dict,en_word_dict,lst_fil_sen,lst_en_sen)
     
 
     # The following code finds out the maximum probability 
@@ -83,8 +96,8 @@ def model_trainer():
     # These maximum probabilities are stored in dictionaries 
     # and saved as .npy files for being used for translation
  
-    total_tagalog_ocurrences = translate_eng_tagalog.shape[0]
-    total_eng_occurrences = translate_eng_tagalog.shape[1]
+    total_tagalog_ocurrences = translate_eng_fil.shape[0]
+    total_eng_occurrences = translate_eng_fil.shape[1]
 
     #final dictionaries for translation mapping
     english_map = {}
@@ -95,11 +108,11 @@ def model_trainer():
         i = 0
         for tagalog_marker in range(total_tagalog_ocurrences):
          #for all English words e do
-            if translate_eng_tagalog[tagalog_marker][eng_marker] > maximum : 
-                maximum = translate_eng_tagalog[tagalog_marker][eng_marker]
+            if translate_eng_fil[tagalog_marker][eng_marker] > maximum : 
+                maximum = translate_eng_fil[tagalog_marker][eng_marker]
                 i = tagalog_marker
 
-        english_map[reverse_english_word_dict[eng_marker]] = reverse_tagalog_word_dict[i]
+        english_map[reverse_en_dict[eng_marker]] = reverse_fil_dict[i]
         #end for
     #end for
     for tagalog_marker in range(total_tagalog_ocurrences): #for all foreign words f do
@@ -107,12 +120,12 @@ def model_trainer():
         i = 0
         for eng_marker in range(total_eng_occurrences):
          #for all English words e do
-            if translate_eng_tagalog[tagalog_marker][eng_marker] > maximum : 
-                maximum = translate_eng_tagalog[tagalog_marker][eng_marker]
+            if translate_eng_fil[tagalog_marker][eng_marker] > maximum : 
+                maximum = translate_eng_fil[tagalog_marker][eng_marker]
                 i = eng_marker
         #end for
-        tagalog_map[reverse_tagalog_word_dict[tagalog_marker]] = reverse_english_word_dict[i]
+        tagalog_map[reverse_fil_dict[tagalog_marker]] = reverse_en_dict[i]
     #end for    
 
-    np.save("trained_data/tagalog_to_english_maximised",tagalog_map)
-    np.save("trained_data/english_to_tagalog_maximised",english_map)
+    np.save("trained_data/tagalog_to_english_maximised", tagalog_map)
+    np.save("trained_data/english_to_tagalog_maximised", english_map)
