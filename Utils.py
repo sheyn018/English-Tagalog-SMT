e266d9f920278b076b8fa8d1bf3b09e4e5063907
HakobCob <102731890+HakobCob@users.noreply.github.com>
2022-08-02 23:17:07 +0800

    Add files via upload

--- Utils.py
+++ Utils.py
@@ -1,34 +1,39 @@
 import math
 
-#max_num_of_iterations = 6
-max_num_of_iterations = 15
+# Max Iterations
+# int_max_iter = 6
+int_max_iter = 15
 const = 0.1
 
-def is_converged(new,old,num_of_iterations):
-    converging_factor = 0.00000001
-    if num_of_iterations > max_num_of_iterations :
+# pnew_trans = trans_en_fil_matrix
+# pold_trans = trans_en_fil_matrix_prev
+def is_converged(pnew_trans, pold_trans, pint_iter):
+    # converging factor
+    float_conv_factor = 0.00000001
+    if pint_iter > int_max_iter:
         return True
 
-    for i in range(len(new)):
-        for j in range(len(new[0])):
-            if math.fabs(new[i][j]- old[i][j]) > converging_factor:
+    # checking if the length of the dictionaries is converging to the converging factor
+    for int_index in range(len(pnew_trans)):
+        for int_index2 in range(len(pnew_trans[0])):
+            if math.fabs(pnew_trans[int_index][int_index2] - pold_trans[int_index][int_index2]) > float_conv_factor:
                 return False
     return True
 
 
-def nCr(n,r):
+def nCr(n, r):
     try:
-        if (n-r < 0) :
+        if (n - r < 0):
             return 1
         f = math.factorial
-        return f(n) / f(r) / f(n-r)
+        return f(n) / f(r) / f(n - r)
     except:
         print("value error " + str(n) + "  " + str(r))
         raise
 
 
 def factorial(n):
-    if n < 0 :
+    if n < 0:
         return 1
     f = math.factorial
     return f(n)
